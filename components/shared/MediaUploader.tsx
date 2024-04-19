5884cee24e9412623ea06d5d023772a619334999
MD REHAN <md.saquib22b@gmail.com>
2024-04-19 22:58:09 +0530

    fixed minor errors

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="visualStorm"
       options={{
         multiple: false,
         resourceType: "image",
